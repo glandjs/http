2add7c4d05c2b388bd6f24ebcc25eb83b86096b3
mahdi <bitsgenix@gmail.com>
2025-05-17 10:30:16 +0330

    refactor: remove fastify packagge

--- packages/fastify/context.ts
+++
@@ -1,4 +0,0 @@
-import { HttpContext } from '@glandjs/http'
-import type { FastifyReply, FastifyRequest } from 'fastify'
-
-export class FastifyContext extends HttpContext<FastifyRequest, FastifyReply> {}
